453daa59e5436be50835fd6eebd6df62e3552546
leon-kwy <leont_kwy@163.com>
2021-11-29 18:56:42 -0500

    1

--- application.py
+++ application.py
@@ -17,22 +17,24 @@ logger.setLevel(logging.INFO)
 OFFSET = 0
 MAXLIMIT = 20
 
+
 # help function for pagination
 def handle_links(url, offset, limit):
     if "?" not in url:
-        url += "?offset=" +str(offset)+"&limit=" +str(limit)
+        url += "?offset=" + str(offset) + "&limit=" + str(limit)
     else:
         if "offset" not in url:
-            url = url + "&offset=" +str(offset)
+            url = url + "&offset=" + str(offset)
         if "limit" not in url:
-            url = url +"&limit=" +str(limit)
+            url = url + "&limit=" + str(limit)
     links = []
-    nexturl = re.sub("offset=\d+","offset="+str(offset+limit), url)
-    prevurl = re.sub("offset=\d+","offset="+str(max(0,offset-limit)), url)
-    links.append({"rel":"self","href":url})
-    links.append({"rel":"next","href":nexturl})
-    links.append({"rel":"prev","href":prevurl})
+    nexturl = re.sub("offset=\d+", "offset=" + str(offset + limit), url)
+    prevurl = re.sub("offset=\d+", "offset=" + str(max(0, offset - limit)), url)
+    links.append({"rel": "self", "href": url})
+    links.append({"rel": "next", "href": nexturl})
+    links.append({"rel": "prev", "href": prevurl})
     return links
+
 
 application = Flask(__name__)
 CORS(application)
@@ -40,7 +42,7 @@ CORS(application)
 
 @application.route('/')
 def hello_world():
-   return '<u>Hello World!</u>'
+    return '<u>Hello World!</u>'
 
 
 @application.route('/Game', methods=['GET', 'POST'])
@@ -53,7 +55,7 @@ def get_game():
             limit = MAXLIMIT
         data = GameResource.find_by_template(None, limit, offset)
         links = handle_links(request.url, offset, limit)
-        res ={"data":data,"links":links}
+        res = {"data": data, "links": links}
         if (data):
             resp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
             return resp
@@ -70,19 +72,19 @@ def get_game():
         type4 = request.form['type4']
         type5 = request.form['type5']
         dev = request.form['dev']
-        if(len(game_name) != 0 and len(type1) != 0 and len(type2) != 0 and len(type3) != 0 and len(type4) != 0 and
+        if (len(game_name) != 0 and len(type1) != 0 and len(type2) != 0 and len(type3) != 0 and len(type4) != 0 and
                 len(type5) != 0 and len(dev) != 0):
 
             create_data = {
-            "Game_id": id,
-            "Game_name": game_name,
-            "Type1": type1,
-            "Type2": type2,
-            "Type3": type3,
-            "Type4": type4,
-            "Type5": type5,
-            "DEVELOPER": dev
-        }
+                "Game_id": id,
+                "Game_name": game_name,
+                "Type1": type1,
+                "Type2": type2,
+                "Type3": type3,
+                "Type4": type4,
+                "Type5": type5,
+                "DEVELOPER": dev
+            }
             res = GameResource.create(create_data)
             print(res)
             ans = GameResource.find_by_template({"Game_name": game_name})
@@ -92,45 +94,48 @@ def get_game():
             # print(request.content)
             resp = Response(json.dumps(succ, default=str), status=201, content_type="application/json")
         else:
-            resp = Response(json.dumps('Status Code: 400 Bad Data', default=str), status=400, content_type="application/json")
+            resp = Response(json.dumps('Status Code: 400 Bad Data', default=str), status=400,
+                            content_type="application/json")
 
             return resp
 
-@application.route('/Game/id/<game_id>', methods = ['GET', 'DELETE'])
+
+@application.route('/Game/id/<game_id>', methods=['GET', 'DELETE'])
 def get_game_by_id(game_id):
     if request.method == 'GET':
         template = {"Game_id": game_id}
         res = GameResource.find_by_template(template)
-        if(res):
+        if (res):
             resp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
             return resp
         else:
-            resp = Response(json.dumps('404 Not Found: No such games.', default = str), status=404,
-                            content_type="application/json" )
+            resp = Response(json.dumps('404 Not Found: No such games.', default=str), status=404,
+                            content_type="application/json")
             return resp
     elif request.method == 'DELETE':
         template = {"Game_id": game_id}
         res = GameResource.delete(template)
-        if(res):
+        if (res):
             resp = Response(json.dumps('Status Code: 204 Delete Successfully', default=str),
                             status=200, content_type="application/json")
             return resp
         else:
-            resp = Response(json.dumps('Status Code: 400 Bad Data', default=str), status=200, content_type="application/json")
+            resp = Response(json.dumps('Status Code: 400 Bad Data', default=str), status=200,
+                            content_type="application/json")
             return resp
 
 
-@application.route('/Game/name/<name>', methods = ['GET', 'DELETE'])
+@application.route('/Game/name/<name>', methods=['GET', 'DELETE'])
 def get_game_by_name(name):
     if request.method == 'GET':
         template = {"Game_name": name}
         res = GameResource.find_by_template(template)
-        if(res):
+        if (res):
             resp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
             return resp
         else:
-            resp = Response(json.dumps('404 Not Found: No such games.', default = str), status=404,
-                            content_type="application/json" )
+            resp = Response(json.dumps('404 Not Found: No such games.', default=str), status=404,
+                            content_type="application/json")
             return resp
     elif request.method == 'DELETE':
         template = {"Game_name": name}
@@ -145,7 +150,7 @@ def get_game_by_name(name):
             return resp
 
 
-@application.route('/Game/type/<type>', methods = ['GET'])
+@application.route('/Game/type/<type>', methods=['GET'])
 def get_game_by_type(type):
     if request.method == 'GET':
         print(type)
@@ -156,7 +161,7 @@ def get_game_by_type(type):
             limit = MAXLIMIT
         data = GameResource.find_by_type(template, limit, offset)
         links = handle_links(request.url, offset, limit)
-        res ={"data":data,"links":links}
+        res = {"data": data, "links": links}
         if (data):
             resp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
             return resp
@@ -165,8 +170,8 @@ def get_game_by_type(type):
                             content_type="application/json")
             return resp
 
-          
-@application.route('/Game/dev/<dev>', methods = ['GET'])
+
+@application.route('/Game/dev/<dev>', methods=['GET'])
 def get_game_by_dev(dev):
     if request.method == 'GET':
         print(dev)
@@ -177,7 +182,7 @@ def get_game_by_dev(dev):
             limit = MAXLIMIT
         data = GameResource.find_by_dev(template, limit, offset)
         links = handle_links(request.url, offset, limit)
-        res ={"data":data,"links":links}
+        res = {"data": data, "links": links}
         if (data):
             resp = Response(json.dumps(res, default=str), status=200, content_type="application/json")
             return resp
